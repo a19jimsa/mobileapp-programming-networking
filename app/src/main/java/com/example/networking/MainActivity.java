6711efba145461261a22ea6034397837270ae276
a19jimsa <a19jimsa@student.his.se>
2021-04-15 11:07:26 +0200

    Added network task code

--- app/src/main/java/com/example/networking/MainActivity.java
+++ app/src/main/java/com/example/networking/MainActivity.java
@@ -2,25 +2,37 @@ package com.example.networking;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.annotation.SuppressLint;
+import android.os.AsyncTask;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.Toast;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.URL;
 import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
-    private ArrayList<Mountain> mountains = new ArrayList<Mountain>();
+    private ArrayList<Mountain> mountains;
+    private ArrayAdapter<Mountain> adapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        mountains = new ArrayList<Mountain>();
         mountains.add(new Mountain("Berg", 1200));
         mountains.add(new Mountain("Berg", 1200));
         mountains.add(new Mountain("Berg", 1200));
-        ArrayAdapter<Mountain> adapter = new ArrayAdapter<Mountain>(this, R.layout.list_item_textview,R.id.list_item_text, mountains);
+        adapter = new ArrayAdapter<Mountain>(this, R.layout.list_item_textview,R.id.list_item_text, mountains);
         ListView my_listView = findViewById(R.id.my_listView);
         my_listView.setAdapter(adapter);
         my_listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -31,4 +43,51 @@ public class MainActivity extends AppCompatActivity {
         });
 
     }
+    @SuppressLint("StaticFieldLeak")
+    private class JsonTask extends AsyncTask<String, String, String> {
+
+        private HttpURLConnection connection = null;
+        private BufferedReader reader = null;
+
+        protected String doInBackground(String... params) {
+            try {
+                URL url = new URL(params[0]);
+                connection = (HttpURLConnection) url.openConnection();
+                connection.connect();
+
+                InputStream stream = connection.getInputStream();
+                reader = new BufferedReader(new InputStreamReader(stream));
+
+                StringBuilder builder = new StringBuilder();
+                String line;
+                while ((line = reader.readLine()) != null && !isCancelled()) {
+                    builder.append(line).append("\n");
+                }
+                return builder.toString();
+            } catch (MalformedURLException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                if (connection != null) {
+                    connection.disconnect();
+                }
+                try {
+                    if (reader != null) {
+                        reader.close();
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String json) {
+            Log.d("TAG", json);
+        }
+    }
+
 }
+
